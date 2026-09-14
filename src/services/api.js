ba94b74b7361df8c465c83604fc4db44a321161a
RND Intern 1 <email@email.com>
2026-09-14 11:33:23 +0800

    updating env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   withXSRFToken: true,
   headers: {
@@ -11,3 +11,5 @@ const api = axios.create({
 });
 
 export default api;
+
+//http://localhost:8080
