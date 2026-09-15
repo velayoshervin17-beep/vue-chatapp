ed489330abb097640d0cf462c114edb972a3666a
RND Intern 1 <email@email.com>
2026-09-15 12:43:23 +0800

    adding interceptor for axios token injection

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,6 +10,19 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use((config) => {
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("XSRF-TOKEN="))
+    ?.split("=")[1];
+
+  if (token) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+  }
+
+  return config;
+});
+
 export default api;
 
 //http://localhost:8080
