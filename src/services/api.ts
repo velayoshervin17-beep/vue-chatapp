29c59767a4a6faa33cba11d60e847dc381229295
RND Intern 1 <email@email.com>
2026-09-15 15:35:31 +0800

    removing api interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,29 +3,12 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  withXSRFToken: true,
+  xsrfCookieName: "XSRF-TOKEN",
+  xsrfHeaderName: "X-XSRF-TOKEN",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
   },
-});
-
-api.interceptors.request.use((config) => {
-  const xsrf = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("XSRF-TOKEN="));
-
-  if (xsrf) {
-    const token = decodeURIComponent(xsrf.substring("XSRF-TOKEN=".length));
-
-    config.headers["X-XSRF-TOKEN"] = token;
-
-    console.log("CSRF HEADER SET");
-  } else {
-    console.log("NO XSRF-TOKEN COOKIE");
-  }
-
-  return config;
 });
 
 export default api;
